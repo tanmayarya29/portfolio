c23ed84eb0fd956cdda01cc6866c8f61208c7d07
Tanmay Mishra <tanmayarya29@gmail.com>
2022-01-16 23:10:10 +0530

    initial

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import "./menu.scss";
+
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu "+(menuOpen && "active")}>
+      <ul>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
